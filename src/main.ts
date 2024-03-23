678b10fb61e00e45bcaa6041fde95163a32808ea
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-03-24 06:33:49 +0700

    refactor: configured CORS

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3001', 'https://okk-meets-postman.vercel.app'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
