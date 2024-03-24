4c5b5c0ad1fdeba88aabd917cb9351d8341c63a6
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-03-24 14:04:45 +0700

    feat: implemented the prisma-related exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,10 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -6,22 +6,29 @@ import { Response } from 'express';
 @Catch(PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
-    console.error(exception.message);
+    console.error(exception.code);
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const message = exception.message.replace(/\n/g, '');
+    const message = exception.message.split('\n').slice(-1)[0];
 
     switch (exception.code) {
-      case 'P2002': {
+      case 'P2003': {
         const status = HttpStatus.CONFLICT;
         response.status(status).json({
-          statusCode: status,
           meesage: message,
+          statusCode: status,
+        });
+        break;
+      }
+      case 'P2025': {
+        const status = HttpStatus.NOT_FOUND;
+        response.status(status).json({
+          meesage: message,
+          statusCode: status,
         });
         break;
       }
       default: {
-        super.catch(exception, host);
         break;
       }
     }
