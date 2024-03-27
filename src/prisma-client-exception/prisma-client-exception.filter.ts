6a64e7ede731080d3749e2f2dc09e7deac4af7b2
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-03-27 10:26:00 +0700

    refactor: add comments

--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -3,12 +3,14 @@ import { BaseExceptionFilter } from '@nestjs/core';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { Response } from 'express';
 
-@Catch(PrismaClientKnownRequestError)
+@Catch(PrismaClientKnownRequestError) // Catch errors because of prisma logic
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
-    console.error(exception.code);
+    console.error(exception.message);
+    // Get the response to be sent back to the request
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+    // Get the last line of the message (the error message)
     const message = exception.message.split('\n').slice(-1)[0];
 
     switch (exception.code) {
