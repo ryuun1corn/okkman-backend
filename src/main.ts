97a43b67568be1bb9d445b0e24b142d80c97772c
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-03-28 19:56:15 +0700

    refactor: prepare for deployment

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
