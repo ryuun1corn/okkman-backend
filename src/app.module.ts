a3fa321af38e919df922007e2d55670fb3e144d7
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-03-28 15:50:05 +0700

    init: speakers and sponsors resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { EventsModule } from './events/events.module';
 import { GroupsModule } from './groups/groups.module';
 import { CommitteesModule } from './committees/committees.module';
 import { MenteesModule } from './mentees/mentees.module';
+import { SponsorsModule } from './sponsors/sponsors.module';
+import { SpeakersModule } from './speakers/speakers.module';
 
 @Module({
-  imports: [EventsModule, GroupsModule, CommitteesModule, MenteesModule],
+  imports: [
+    EventsModule,
+    GroupsModule,
+    CommitteesModule,
+    MenteesModule,
+    SponsorsModule,
+    SpeakersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
